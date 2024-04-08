1d0c197098ad08c0e2eda0f9e5c3b2d12b166f57
Nastya B <nastybondareva@yandex.ru>
2024-04-08 23:03:52 +0300

    main changed

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,45 @@
+import java.io.BufferedOutputStream;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        openZip("D://Games/savegames/saveGames.zip", "D://Games/savegames/");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+    }
+    public static void openZip (String addressZip, String puth) {
+        try (ZipInputStream zin = new ZipInputStream(new FileInputStream(addressZip))) {
+            ZipEntry entry;
+            String name;
+            while ((entry = zin.getNextEntry())  != null) {
+                name = entry.getName(); // получим название файла распаковка
+                try (BufferedOutputStream bos = new BufferedOutputStream(new FileOutputStream(puth + name))) {
+                    for (int c = zin.read(); c != -1; c = zin.read()) {
+                        bos.write(c);
+                    }
+                    zin.closeEntry();
+                }
+            }
+        } catch (Exception ex) {
+            System.out.println(ex.getMessage());
         }
     }
+
+    public static String openProgress (String addressFile){
+        try (FileInputStream fis = new FileInputStream("save.dat");
+             ObjectInputStream ois = new ObjectInputStream(fis)) {
+// десериализуем объект и скастим его в класс
+            gameProgress = (GameProgress) ois.readObject();
+        } catch (Exception ex) {
+            System.out.println(ex.getMessage());
+        }
+        System.out.println(gameProgress);
+
+    }
+
 }
