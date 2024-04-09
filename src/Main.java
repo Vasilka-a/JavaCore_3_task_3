72206cdc9637c02c3df73b644343083c67af3c98
Nastya B <nastybondareva@yandex.ru>
2024-04-09 12:24:54 +0300

    main changed

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,4 @@
-import java.io.BufferedOutputStream;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
+import java.io.*;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipInputStream;
 
@@ -9,15 +6,21 @@ import java.util.zip.ZipInputStream;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+
         openZip("D://Games/savegames/saveGames.zip", "D://Games/savegames/");
 
+        openProgress("D://Games/savegames/save1.dat");
+        openProgress("D://Games/savegames/save2.dat");
+        openProgress("D://Games/savegames/save3.dat");
+
     }
-    public static void openZip (String addressZip, String puth) {
+
+    public static void openZip(String addressZip, String puth) {
         try (ZipInputStream zin = new ZipInputStream(new FileInputStream(addressZip))) {
             ZipEntry entry;
             String name;
-            while ((entry = zin.getNextEntry())  != null) {
-                name = entry.getName(); // получим название файла распаковка
+            while ((entry = zin.getNextEntry()) != null) {
+                name = entry.getName();
                 try (BufferedOutputStream bos = new BufferedOutputStream(new FileOutputStream(puth + name))) {
                     for (int c = zin.read(); c != -1; c = zin.read()) {
                         bos.write(c);
@@ -30,16 +33,17 @@ public class Main {
         }
     }
 
-    public static String openProgress (String addressFile){
-        try (FileInputStream fis = new FileInputStream("save.dat");
+    public static void openProgress(String addressFile) {
+        GameProgress gameProgress = null;
+        try (FileInputStream fis = new FileInputStream(addressFile);
              ObjectInputStream ois = new ObjectInputStream(fis)) {
-// десериализуем объект и скастим его в класс
             gameProgress = (GameProgress) ois.readObject();
+        } catch (FileNotFoundException ex) {
+            System.out.println(ex.getMessage());
         } catch (Exception ex) {
             System.out.println(ex.getMessage());
         }
         System.out.println(gameProgress);
-
     }
-
 }
+
